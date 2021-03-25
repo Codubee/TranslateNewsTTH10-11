c1d433717a24834ba12a23724467e1fe541876cf
khushi jogani <joganikhushi@gmail.com>
2021-03-24 21:08:25 -0500

    TTH6 - Create a navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import LandingPage from './views/LandingPage'
+import News from './views/News'
+import MyNavBar from './components/Navbar'
 function App() {
   return (
-    <LandingPage/>
+    <MyNavBar/>
   );
 }
 
---
+++ frontend/src/views/News.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function News(){
+    return(
+        <div>
+            {/* Insert your created components here */}
+            <h1>News Page Component</h1>
+        </div>
+    )
+}
+
+export default News;
