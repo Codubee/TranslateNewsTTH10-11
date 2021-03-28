c43df8a1c0feaff2bf3a1e052dcce1bfb66d0b53
anisharajuu <anisharaju@gmail.com>
2021-03-28 11:22:36 -0500

    Added feature component

---
+++ frontend/src/components/Features.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Features() {
+  return (
+    <div>
+      <h1>
+        Features Include:
+      </h1>
+      <p> • Have translated news into different languages</p>
+      <p> • Helping people get critical information</p>
+    </div >
+  )
+};
+
+export default Features;
--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 //import TeamComponent
 import Team from '../components/Team'
+import Features from '../components/Features'
 
 function LandingPage(){
     return(
         <div>
+            <Features/>
             <Team/>
         </div>
     )
