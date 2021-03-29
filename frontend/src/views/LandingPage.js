1b2a722fd85d42aaa8ee16bbd4f24baaf90cd425
galJane <59939491+galJane@users.noreply.github.com>
2021-03-29 00:04:07 -0500

    Jumbotron to welcome users and brief introduction.

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+//import Explanation Componet
+import Explanation from '../components/Explanation';
 //import TeamComponent
 import Team from '../components/Team'
 
 function LandingPage(){
     return(
         <div>
+            <Explanation/>
             <Team/>
         </div>
     )
