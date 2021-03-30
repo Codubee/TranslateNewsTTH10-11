e01bb0cb36ed24c3bfb12d5762171fd2902da02c
anisharajuu <anisharaju@gmail.com>
2021-03-29 22:57:10 -0500

    Centered text in middle of screen

--- frontend/src/components/Features.js
+++ frontend/src/components/Features.js
@@ -3,12 +3,15 @@ import React from 'react';
 function Features() {
   return (
     <div>
+    <center>
       <h1>
         Features Include:
       </h1>
       <p> • Have translated news into different languages</p>
       <p> • Helping people get critical information</p>
+    </center>
     </div >
+
   )
 };
 
