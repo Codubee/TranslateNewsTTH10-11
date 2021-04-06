8b0ba7656662c43f0b566b4841f31676f8e234e6
bushra_h <bushra.h818@gmail.com>
2021-04-06 10:28:46 -0500

    select user language

---
+++ frontend/src/components/Languages.js
@@ -0,0 +1,104 @@
+import React from 'react' 
+
+function Lang() { 
+    return (
+        //user can select their preferred language to read their news articles
+        <select data-placeholder="Select a Language">
+            <option value="af">Afrikaans</option>
+            <option value="sq">Albanian</option>
+            <option value="am">Amharic</option>
+            <option value="ar">Arabic</option>
+            <option value="hy">Armenian</option>
+            <option value="as">Assamese</option>
+            <option value="az">Azerbaijani</option>
+            <option value="bn">Bangla</option>
+            <option value="bs">Bosnian (Latin)</option>
+            <option value="bg">Bulgarian</option>
+            <option value="yue">Cantonese (Traditional)</option>
+            <option value="ca">Catalan</option>
+            <option value="zh-Hans">Chinese Simplified</option>
+            <option value="zh-Hant">Chinese Traditional</option>
+            <option value="hr">Croatian</option>
+            <option value="cs">Czech</option>
+            <option value="km">Cambodian</option> 
+            <option value="da">Danish</option>
+            <option value="nl">Dutch</option>
+            <option value="en">English</option>
+            <option value="et">Estonian</option>
+            <option value="fj">Fijian</option>
+            <option value="fil">Filipino</option>
+            <option value="fi">Finnish</option>
+            <option value="fr">French</option>
+            <option value="fr-ca">French (Canadian)</option>
+            <option value="de">German</option>
+            <option value="el">Greek</option>
+            <option value="gu">Gujarati</option>
+            <option value="ht">Hatian Creole</option>
+            <option value="he">Hebrew</option>
+            <option value="hi">Hindi</option>
+            <option value="mww">Hmong Daw</option>
+            <option value="hu">Hungarian</option>
+            <option value="is">Icelandic</option>
+            <option value="id">Indonesian</option>
+            <option value="iu">Inukitut</option>
+            <option value="ga">Irish</option>
+            <option value="it">Italian</option>
+            <option value="ja">Japanese</option>
+            <option value="kd">Kannada</option>
+            <option value="kk">Kazakh</option>
+            <option value="km">Khmer</option>
+            <option value="tlh-Latn">Klingon</option>
+            <option value="tlh-Piqd">Klingon (plqaD)</option>
+            <option value="ko">Korean</option>
+            <option value="ku">Kurdish (Central)</option>
+            <option value="kmr">Kurdish (Northern)</option>
+            <option value="lo">Lao</option>
+            <option value="lv">Latvian</option>
+            <option value="lt">Lithuanian</option>
+            <option value="mk">Macedonian</option>
+            <option value="mg">Malagasy</option>
+            <option value="ms">Malay</option>
+            <option value="ml">Malayalam</option>
+            <option value="mt">Maltese</option>
+            <option value="mi">Maori</option>
+            <option value="mr">Marathi</option>
+            <option value="my">Mayanmar</option>
+            <option value="mn">Mongolian</option>
+            <option value="ne">Nepali</option>
+            <option value="nb">Norwegian</option>
+            <option value="or">Odia</option>
+            <option value="ps">Pashto</option>
+            <option value="fa">Persian</option>
+            <option value="pl">Polish</option>
+            <option value="pt">Portuguese (Brazil)</option>
+            <option value="pt-pt">Portuguese (Portugal)</option>
+            <option value="pa">Punjabi</option>
+            <option value="otq">Queretaro Otomi</option>
+            <option value="ro">Romanian</option>
+            <option value="ru">Russian</option>
+            <option value="sm">Samoan</option>
+            <option value="sr-Latn">Serbian (Latin)</option>
+            <option value="sk">Slovak</option>
+            <option value="sl">Slovenian</option>
+            <option value="es">Spanish</option>
+            <option value="sw">Swahili</option>
+            <option value="sv">Swedish </option>
+            <option value="ty">Tahitian</option>
+            <option value="ta">Tamil</option>
+            <option value="te">Telugu</option>
+            <option value="th">Thai</option>
+            <option value="ti">Tigrinya</option>
+            <option value="bo">Tibetan</option>
+            <option value="to">Tonga</option>
+            <option value="tr">Turkish</option>
+            <option value="uk">Ukrainian</option>
+            <option value="ur">Urdu</option>
+            <option value="uz">Uzbek</option>
+            <option value="vi">Vietnamese</option>
+            <option value="cy">Welsh</option>
+            <option value="yua">Yucatec Maya</option>
+        </select>
+    )
+}
+
+export default Lang
