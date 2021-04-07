9916f541d86245a0283718eeed9f9e6e9ca4423c
galJane <59939491+galJane@users.noreply.github.com>
2021-04-07 16:38:56 -0500

    Added React Routing for Navbar and Created MatchPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import MyNavBar from './components/Navbar';
 import LandingPage from './views/LandingPage'
+import MatchPage from './views/MatchPage'
 import Navbar from './components/Navbar'
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 function App() {
   return (
+    //Allows us to switch between pages with the Navbar using React Router.
     <div>
     <Navbar/>
-      <LandingPage/>
+    <Router>
+    <Switch>
+    <Route path="/News">
+        <MatchPage/>
+      </Route>
+      <Route path="/">
+        <LandingPage/>
+      </Route>
+    </Switch>
+    </Router>
     </div>
   );
 }
---
+++ frontend/src/views/MatchPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//The heading was just to test the routing, you can erase it when you start adding components.
+function MatchPage(){
+    return(
+        <h1>Match Page</h1>
+    )
+}
+export default MatchPage;
