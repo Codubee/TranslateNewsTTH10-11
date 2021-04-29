6355baa95325da0ac8f98db7cb334425713b134d
Anisha Raju <73310478+anisharajuu@users.noreply.github.com>
2021-04-29 15:42:22 -0700

    TTH-22 - backend for weather (#21)
    * added backend for weather
    
    * added backend for weather
    
    * added backend for weather
    
    * added backend for weather

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const app = express()
 const axios = require('axios');
 app.use(express.json());
 
-app.get('/getVaccineNews', function (req, res) {
 
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getVaccineNews')
+app.get('/getWeather', function (req, res) {
+
+    axios.get('https://codubee-projects-api.herokuapp.com/translate/getWeather')
    .then(function (response) {
         // handle success and send back a 200 response with the data
         console.log(response.data);
@@ -16,33 +17,4 @@ app.get('/getVaccineNews', function (req, res) {
         res.status(400).json({error:"An error occurred"});
     })
 })
-
-app.get('/getLocalNews', function (req, res) {
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getLocalNews')
-
-    .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
-
-app.get('/getNationalNews', function (req, res) {
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getNationalNews')
-    .then(function(response){
-        //handle success and send back 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        //error message
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
-
 app.listen(8080, () => console.log('Listening at locahost:8080'))
