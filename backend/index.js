af8745f13405fdbc0ef52479a31ed9c616a525d7
Anisha Raju <anisharaju@gmail.com>
2021-04-29 01:14:24 -0700

    added backend for weather

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const app = express()
 const axios = require('axios');
 app.use(express.json());
 
-app.get('/getVaccineNews', function (req, res) {
+app.get('/getWeather', function (req, res) {
 
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getVaccineNews')
+    axios.get('https://codubee-projects-api.herokuapp.com/translate/getWeather')
    .then(function (response) {
         // handle success and send back a 200 response with the data
         console.log(response.data);
@@ -16,47 +14,3 @@ app.get('/getVaccineNews', function (req, res) {
         res.status(400).json({error:"An error occurred"});
     })
 })
-
-app.get('/getLocalNews', function (req, res) {
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getLocalNews')
-
-    .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
-
-app.get('/getNationalNews', function (req, res) {
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getNationalNews')
-    .then(function(response){
-        //handle success and send back 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        //error message
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
-
-app.get('/getNationalNews', function (req, res) {
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getWeather')
-    .then(function(response){
-        //handle success and send back 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        //error message
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
-
-app.listen(8080, () => console.log('Listening at locahost:8080'))
