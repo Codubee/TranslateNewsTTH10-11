1aec5cf998be17b6477b510c3986d08f583efeda
galJane <59939491+galJane@users.noreply.github.com>
2021-04-28 21:41:07 -0500

    backend route for National News (#19)

--- backend/index.js
+++ backend/index.js
@@ -30,4 +30,19 @@ app.get('/getLocalNews', function (req, res) {
         res.status(400).json({error:"An error occurred"});
     })
 })
+
+app.get('/getNationalNews', function (req, res) {
+    axios.get('https://codubee-projects-api.herokuapp.com/translate/getNationalNews')
+    .then(function(response){
+        //handle success and send back 200 response with the data
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        //error message
+        console.log(error)
+        res.status(400).json({error:"An error occurred"});
+    })
+})
+
 app.listen(8080, () => console.log('Listening at locahost:8080'))
