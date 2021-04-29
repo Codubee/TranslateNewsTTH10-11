53cc76fcc26e6f321ada60bc10aa7f2f172bc860
Soundarya Baskar <74214015+soundaryabaskar@users.noreply.github.com>
2021-04-28 21:39:51 -0500

    Backend route for vaccine news (#18)
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,23 @@ const app = express()
 const axios = require('axios');
 app.use(express.json());
 
+app.get('/getVaccineNews', function (req, res) {
+
+    axios.get('https://codubee-projects-api.herokuapp.com/translate/getVaccineNews')
+   .then(function (response) {
+        // handle success and send back a 200 response with the data
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        console.log(error)
+        res.status(400).json({error:"An error occurred"});
+    })
+})
+
 app.get('/getLocalNews', function (req, res) {
     axios.get('https://codubee-projects-api.herokuapp.com/translate/getLocalNews')
+
     .then(function (response) {
         // handle success and send back a 200 response with the data
         console.log(response.data);
