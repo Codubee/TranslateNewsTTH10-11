23e2ac80429c9d97c468519b2d21363e4954d0dd
Anisha Raju <anisharaju@gmail.com>
2021-04-29 01:04:05 -0700

    added backend for weather

--- backend/index.js
+++ backend/index.js
@@ -45,4 +45,18 @@ app.get('/getNationalNews', function (req, res) {
     })
 })
 
+app.get('/getNationalNews', function (req, res) {
+    axios.get('https://codubee-projects-api.herokuapp.com/translate/getWeather')
+    .then(function(response){
+        //handle success and send back 200 response with the data
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        //error message
+        console.log(error)
+        res.status(400).json({error:"An error occurred"});
+    })
+})
+
 app.listen(8080, () => console.log('Listening at locahost:8080'))
