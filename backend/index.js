14e4fe3dd32d9007306b30c7b39bc8d18792f015
bushra_h <bushra.h818@gmail.com>
2021-04-30 16:30:48 -0500

    translate the article to the user specified language

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,29 @@
 const express = require('express')
 const app = express()
 const axios = require('axios');
-app.use(express.json());
+app.use(express.json()); //reads request post data
 
-app.get('/getVaccineNews', function (req, res) {
+// This takes in a langauge query parameter
+// An example request is localhost:8080/translateNews?language=es
+app.post('/translateNews', function (postmanReq, postmanRes) {
 
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getVaccineNews')
-   .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
+    // Log the query parameters
+    console.log(postmanReq.query) 
+    let language = postmanReq.query.language;
 
-app.get('/getLocalNews', function (req, res) {
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getLocalNews')
+   console.log(postmanReq.body)
 
+    //Make a post request with the name query parameter
+    axios.post('https://codubee-projects-api.herokuapp.com/translate/translateNews?language='+ language, postmanReq.body)
     .then(function (response) {
         // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
+        console.log(response.data); //printing terminal
+        postmanRes.status(200).json(response.data);
     })
     .catch(function (error) {
         console.log(error)
-        res.status(400).json({error:"An error occurred"});
+        postmanRes.status(400).json({error:"An error occurred"});
     })
 })
 
-app.get('/getNationalNews', function (req, res) {
-    axios.get('https://codubee-projects-api.herokuapp.com/translate/getNationalNews')
-    .then(function(response){
-        //handle success and send back 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        //error message
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
-
-app.listen(8080, () => console.log('Listening at locahost:8080'))
+app.listen(8080, () => console.log(`Example app listening at localhost:8080`))
