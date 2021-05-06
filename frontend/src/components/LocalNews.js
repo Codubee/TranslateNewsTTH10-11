2235bbff89c3d51c2a07da0c89294c75682b09d2
khushi jogani <joganikhushi@gmail.com>
2021-05-06 10:29:47 -0500

    Local News file calling getLocalNews api

--- frontend/src/components/LocalNews.js
+++ frontend/src/components/LocalNews.js
@@ -2,50 +2,57 @@ import React from 'react';
 import { Card } from "react-bootstrap";
 import { Row, Col } from 'reactstrap';
 import '../styles/LocalNews.css'
-
-function LocalNews() {
+import axios from "axios";
+class LocalNews extends React.Component {
     // Create an array of javascript objects
-    const LocalNews = [
-        {
-           "image": "https://lh3.googleusercontent.com/pw/ACtC-3d3ip9JeN5zEn_sjeFCOVBvy3Xg0Ys9sRsmAU6-kMj3faiekxvUoyrORlkTrsk_jCTgLFFRkd3PGavF5rdsdUnNAV8iMJio2p4vz0XUBx5MOdvWaDbiLa4CvjG_NIxfQZpGmRlON3RT7B7TRDNqfrTzFg=w1420-h710-no?authuser=0",
-            "title": "Medical News Today",
-            "subtext": "COVID-19 Vaccines: Live Updates",
-            "url": "https://www.medicalnewstoday.com/articles/covid-19-vaccine-live-updates#1"
-        },
-        {
-            "image": "https://lh3.googleusercontent.com/pw/ACtC-3cpdwvlI3sk3xL0pzSaKXWEWnlKjD28QnUdSsLKnVEiWFPv2w8nB2Nh-YlKofgY76_o_vzcK7LNmTHqJZbIObcyuNmHDM-oeSswH8TaHthkVhSmKoPZFuj4hcT2CfLdXAchLWHps0srwkeUwEvXwvN3uw=w2000-h727-no?authuser=0",
-            "title": "Worldometer",
-            "subtext": "Live Updates on Coronavirus Cases, Deaths, and Recovered",
-            "url": "https://www.worldometers.info/coronavirus/"
-        },
-    ]
-    return (
+    constructor(props) {
+        super(props);
+        const LocalNews = [
+            {
+                "text": "",
+                "url": ""
+            },
+        ]
+        this.state = {LocalNews};
+        console.log(this.state);
+    }
+    componentDidMount(){
+        axios.get("/getLocalNews")
+            .then((response) => {
+                console.log(response);
+                this.setState({
+                    text: response.data.text,
+                    url: response.data.url
+                })
+            })
+    }
 
-        <div className="local-container">
-            <h1>Local News</h1>
-            <Row>
-                {
-                    //map to display the articles in cards
-                    LocalNews.map((objLocalNews, id) => (
-                        <div key={id}>
-                            {
-                                < Col sm='6' md='4' key={id}>
-                                    <Card onClick={() => {
-                                        window.open(objLocalNews.url)
+    render() {
+        return (
+            <div className="local-container">
+                <h1>Local News</h1>
+                <Row>
+                    {
+                        //map to display the articles in cards
+                        this.state.LocalNews.map((article, id) => (
+                            <div key={id}>
+                                {
+                                    < Col sm='6' md='4' key={id}>
+                                        <Card onClick={() => {
+                                            window.open(article.url)
                                         }} style={{ width: "280px" }} className="local-box"
-                                    >
-                                        <Card.Img className="local-image" variant="top" src={objLocalNews.image} />
-                                        <Card.Body>
-                                            <Card.Title id="local-title-text">{objLocalNews.title}</Card.Title>
-                                            <Card.Subtitle id="local-subtitle-text" tag="h6" className="text-muted">{objLocalNews.subtext}</Card.Subtitle>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>}
-                        </div>
-                    ))
-                }
-            </Row>
-        </div>
-    )
+                                        >
+                                            <Card.Body>
+                                                <Card.Title id="local-title-text">{article.text}</Card.Title>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>}
+                            </div>
+                        ))
+                    }
+                </Row>
+            </div> 
+        )
+    }
 }
 export default LocalNews;
