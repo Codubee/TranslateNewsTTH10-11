63e12896753cac588d2e063499950865d3f36f9d
Bushra <43095729+bushra-h@users.noreply.github.com>
2021-05-07 14:14:53 -0500

    [TTH-20]Translate the article to the user specified language (#22)
    * translate the article to the user specified language
    
    * translate news backend
    
    Co-authored-by: codubee <info@codubee.com>

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
 const express = require('express')
 const app = express()
 const axios = require('axios');
-app.use(express.json());
+app.use(express.json()); //reads request post data
+
+// This takes in a langauge query parameter
+// An example request is localhost:8080/translateNews?language=es
+app.post('/translateNews', function (postmanReq, postmanRes) {
+
+    // Log the query parameters
+    console.log(postmanReq.query) 
+    let language = postmanReq.query.language;
+
+   console.log(postmanReq.body)
+
+    //Make a post request with the language query parameter
+    axios.post('https://codubee-projects-api.herokuapp.com/translate/translateNews?language='+ language, postmanReq.body)
+    .then(function (response) {
+        // handle success and send back a 200 response with the data
+        console.log(response.data); //printing terminal
+        postmanRes.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        console.log(error)
+        postmanRes.status(400).json({error:"An error occurred"});
+    })
+})
 
 app.get('/getVaccineNews', function (req, res) {
     axios.get('https://codubee-projects-api.herokuapp.com/translate/getVaccineNews')
