d633291d28ad1557bdf76fd16f70df485d304164
Soundarya Baskar <74214015+soundaryabaskar@users.noreply.github.com>
2021-06-06 20:32:48 -0500

    call getVaccineNews API (#25)

--- frontend/src/components/VaccineNews.js
+++ frontend/src/components/VaccineNews.js
@@ -2,48 +2,64 @@ import React from 'react';
 import { Card } from "react-bootstrap";
 import { Row, Col } from 'reactstrap';
 import '../styles/VaccineNews.css'
+import axios from "axios";
+import Loading from './Loading';
 
-function VaccineNews() {
+class VaccineNews extends React.Component {
     // Create an array of javascript objects
-    const VaccineNews = [
-        {
-            "image": "https://lh3.googleusercontent.com/pw/ACtC-3d3ip9JeN5zEn_sjeFCOVBvy3Xg0Ys9sRsmAU6-kMj3faiekxvUoyrORlkTrsk_jCTgLFFRkd3PGavF5rdsdUnNAV8iMJio2p4vz0XUBx5MOdvWaDbiLa4CvjG_NIxfQZpGmRlON3RT7B7TRDNqfrTzFg=w1420-h710-no?authuser=0",
-            "title": "Medical News Today",
-            "subtext": "COVID-19 Vaccines: Live Updates",
-            "url": "https://www.medicalnewstoday.com/articles/covid-19-vaccine-live-updates#1"
-        },
-        {
-            "image": "https://lh3.googleusercontent.com/pw/ACtC-3cpdwvlI3sk3xL0pzSaKXWEWnlKjD28QnUdSsLKnVEiWFPv2w8nB2Nh-YlKofgY76_o_vzcK7LNmTHqJZbIObcyuNmHDM-oeSswH8TaHthkVhSmKoPZFuj4hcT2CfLdXAchLWHps0srwkeUwEvXwvN3uw=w2000-h727-no?authuser=0",
-            "title": "Worldometer",
-            "subtext": "Live Updates on Coronavirus Cases, Deaths, and Recovered ",
-            "url": "https://www.worldometers.info/coronavirus/"
-        },
-    ]
-    return (
-        <div className="vaccine-container">
-            <h1>Vaccine News</h1>
-            <Row>
-                {
-                    VaccineNews.map((objVaccine, id) => (
-                        <div key={id}>
-                            {//opens article link in a new tab when clicked on card
-                                < Col sm='6' md='4' key={id}>
-                                     <Card onClick={() => {
-                                        window.open(objVaccine.url)
-                                        }} style={{ width: "280px", height:"400px" }} className="vaccine-box"
-                                    >
-                                        <Card.Img className="vaccine-image" variant="top" src={objVaccine.image} />
-                                        <Card.Body>
-                                            <Card.Title id="vaccine-title-text">{objVaccine.title}</Card.Title>
-                                            <Card.Subtitle id="vaccine-subtitle-text" tag="h6" className="mb-2 text-muted">{objVaccine.subtext}</Card.Subtitle>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>}
-                        </div>
-                    ))
-                }
-            </Row>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+        this.state = { VaccineNews: [], isLoading: true };
+    }
+    componentDidMount() {
+        axios.get("/getVaccineNews")
+            .then((response) => {
+                console.log(response);
+                this.setState({
+                    VaccineNews: response.data,
+                    isLoading: false
+                })
+            })
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <div>
+                    <h1>Vaccine News</h1>
+                    <Loading />
+                </div>
+            )
+
+        }
+        else {
+            return (
+                <div className="vaccine-container">
+                    <h1>Vaccine News</h1>
+                    <Row>
+                        {
+                            //map to display the articles in cards
+                            this.state.VaccineNews.map((article, id) => (
+                                <div key={id}>
+                                    {
+                                        < Col sm='6' md='4' key={id}>
+                                            <Card onClick={() => {
+                                                window.open(article.url)
+                                            }} style={{ width: "280px" }} className="vaccine-box"
+                                            >
+                                                <Card.Body>
+                                                    <Card.Title id="vaccine-title-text">{article.text}</Card.Title>
+                                                </Card.Body>
+                                            </Card>
+                                        </Col>}
+                                </div>
+                            ))
+                        }
+                    </Row>
+                </div>
+            )
+        }
+
+    }
 }
-export default VaccineNews; 
+export default VaccineNews;
